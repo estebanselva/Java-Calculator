2b1f899fba17f8ee37be56508f47f75b4c4d2d7c
estebanselva <eselvacastillo@gmail.com>
2015-03-05 21:29:28 -0600

    second commit: I have laid out the grid for the button design and had an array and loop set to number the 10 buttons. Only one button is up on this commit.

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,7 +1,5 @@
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import java.awt.*;
+import javax.swing.*;
 
 public class Calculator extends JPanel {
     private static final long serialVersionUID = 1L; // Java convention
@@ -11,11 +9,34 @@ public class Calculator extends JPanel {
 
     public Calculator() {
         setPreferredSize(new Dimension(WIDTH, HEIGHT)); // Sets size of calculator
+
+        GridBagLayout layout = new GridBagLayout();
+        // Setting widths of the grid to 80 because I set the
+        // calculator to 320, so I'm splitting them into 4 columns of 80.
+        layout.columnWidths = new int[] {80, 80, 80, 80};
+        // Setting up row heights to 6 rows of size 80.
+        layout.rowHeights = new int[] {80, 80, 80, 80, 80, 80};
+        setLayout(layout);
+
+        JButton[] numberButtons = new JButton[10];
+
+        // This will set up the buttons so that JButton[0] in the array will be button 1.
+        // JButton[1] will be button 2, and so on.... until all 10 buttons are classified.
+        for(int i = 0; i < numberButtons.length; i++) {
+            numberButtons[i] = new JButton("" + i);
+        }
+
+        GridBagConstraints gbc = new GridBagConstraints(); // Allows us to position our buttons on the screen
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.fill = GridBagConstraints.BOTH;
+        add(numberButtons[0], gbc);
     }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Calculator"); // Make a new frame with the name "Calculator"
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Program will terminate when window is closed.
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // Program will terminate when window is closed.
         frame.setResizable(false);
         frame.setLayout(new BorderLayout()); // This layout allows us to center frame on the screen.
         frame.add(new Calculator(), BorderLayout.CENTER);
